512cc7c2786d29fcba72b97c26169b3409abd5f5
Felipe Lima <felipe.lima@gmail.com>
2014-08-28 16:02:21 -0300

    fixes changelog format and deploy script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-1.0.24
+# 1.0.24
 
-# Updates dependencies (build tools version, app compat, etc)
+* Updates dependencies (build tools version, app compat, etc)
--- mavenLocalDeploy.sh
+++ mavenLocalDeploy.sh
@@ -1,19 +1,20 @@
 mvn install:install-file \
 -DgroupId=com.felipecsl \
 -DartifactId=asymmetricgridview \
--Dversion=1.0.$1-SNAPSHOT \
+-Dversion=$1 \
 -DgeneratePom=true \
 -Dpackaging=aar \
 -Dfile=library/build/outputs/aar/library.aar \
 -DlocalRepositoryPath=/Users/felipecsl/Data/Projects/m2repository/
 
-mv /Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/1.0.$1-SNAPSHOT/maven-metadata-local.xml \
-/Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/1.0.$1-SNAPSHOT/maven-metadata.xml
+mv /Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/$1/maven-metadata-local.xml \
+/Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/$1/maven-metadata.xml
 
 mv /Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/maven-metadata-local.xml \
 /Users/felipecsl/Data/Projects/m2repository/com/felipecsl/asymmetricgridview/maven-metadata.xml
 
 cd /Users/felipecsl/Data/Projects/m2repository/
 
-git ci -am 'Updates AsymmetricGridView to 1.0.$1-SNAPSHOT'
+git add .
+git ci -am 'Updates AsymmetricGridView to $1'
 git push felipecsl master
